26a7bafbd9e8abbf2a5a3dec26b31ea8bb40acf4
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-04 12:18:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-  while number > 0
+  while number > 0 do 
   puts "#{number} SECOND(S)!"
-  number -=
-
+  number -=1
+end
 return "HAPPY NEW YEAR!"
 end
